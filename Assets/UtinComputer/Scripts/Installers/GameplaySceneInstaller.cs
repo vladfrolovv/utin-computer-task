47cfd8744b0870b890c2ee54b437258d2626bbd9
Vlad Frolov <73vladfrolov73@gmail.com>
2026-08-25 15:12:01 +0300

    play button animation

---
+++ Assets/UtinComputer/Scripts/Installers/GameplaySceneInstaller.cs
@@ -0,0 +1,10 @@
+﻿using Zenject;
+namespace UtinComputer.Installers
+{
+    public class GameplaySceneInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+        }
+    }
+}
---
+++ Assets/UtinComputer/Scripts/UI/PlayButtonView.cs
@@ -0,0 +1,35 @@
+﻿using DG.Tweening;
+using UnityEngine;
+using UnityEngine.EventSystems;
+namespace UtinComputer.UI
+{
+    public class PlayButtonView : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+    {
+        [SerializeField] private RectTransform foregroundRectTransform;
+        [SerializeField] private RectTransform backgroundRectTransform;
+
+        [Header("Animation Settings")]
+        [SerializeField] private float moveDownTime = .16f;
+        [SerializeField] private float moveUpTime = .16f;
+
+        private float _foregroundYAmplitude;
+        private Tween _foregroundTween;
+
+        private void Awake()
+        {
+            _foregroundYAmplitude = backgroundRectTransform.anchoredPosition.y;
+        }
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            _foregroundTween?.Kill();
+            _foregroundTween = foregroundRectTransform.DOLocalMoveY(_foregroundYAmplitude, moveDownTime).SetEase(Ease.OutQuint);
+        }
+
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            _foregroundTween?.Kill();
+            _foregroundTween = foregroundRectTransform.DOLocalMoveY(0, moveDownTime).SetEase(Ease.OutQuint);
+        }
+    }
+}
