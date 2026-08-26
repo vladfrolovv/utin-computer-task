4814246034cd4e42406d718bca3e1af607271412
Vlad Frolov <73vladfrolov73@gmail.com>
2026-08-26 14:35:06 +0300

    removed configs installer

--- Assets/UtinComputer/Scripts/Installers/SpheresContextInstaller.cs
+++ Assets/UtinComputer/Scripts/Installers/SpheresContextInstaller.cs
@@ -8,11 +8,14 @@ namespace UtinComputer.Installers
     {
         [SerializeField] private MainSphereView mainSphereView;
         [SerializeField] private ShotSphereView shotSphereView;
+        [SerializeField] private SphereConfig sphereConfig;
 
         public override void InstallBindings()
         {
             Container.Bind<MainSphereView>().FromInstance(mainSphereView).AsSingle();
             Container.Bind<ShotSphereView>().FromInstance(shotSphereView).AsSingle();
+
+            Container.Bind<SphereConfig>().FromInstance(sphereConfig).AsSingle();
 
             Container.BindInterfacesAndSelfTo<ChargeController>().AsSingle();
         }
